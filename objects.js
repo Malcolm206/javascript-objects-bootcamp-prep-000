e763cdc14f0913fc609709c0e633e56332642b88
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-09-11 20:24:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,6 @@ function updatePlaylist(object, artist, song) {
   return Object.assign({}, object, { [artist]: song })
 }
 function removeFromPlaylist(object, artistName) {
-  return delete object.artistName
+  delete object.artistName
+  return object
 }
